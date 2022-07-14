ace0e8a8e2143788e437c4cf5472a62a4d0bbf41
oraz <oraz@redhat.com>
2022-07-14 15:23:15 +0300

    Use two commands to set up OCP annotations

--- distgit/containers/self-node-remediation-operator-bundle/test_render_templates.sh
+++ distgit/containers/self-node-remediation-operator-bundle/test_render_templates.sh
@@ -2,7 +2,7 @@
 
 # get current values from upstream_sources.yaml
 export CI_SELF_NODE_REMEDIATION_UPSTREAM_URL=https://github.com/medik8s/self-node-remediation
-export CI_SELF_NODE_REMEDIATION_UPSTREAM_COMMIT=9db21477b8709f228b54190b93fbf4bb0dd3493a
+export CI_SELF_NODE_REMEDIATION_UPSTREAM_COMMIT=0eb8d6ca7aa1e65371bcc4e6f89fde3758341455
 
 # see gcp_env/product_version
 export CI_VERSION=0.4.0
