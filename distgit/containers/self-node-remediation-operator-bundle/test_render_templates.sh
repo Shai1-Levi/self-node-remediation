8381663c34862b0df8e840fea864c24a6b73162b
razo7 <oraz@redhat.com>
2022-11-04 09:52:44 +0200

    Update test_render_templates commit and CI version

--- distgit/containers/self-node-remediation-operator-bundle/test_render_templates.sh
+++ distgit/containers/self-node-remediation-operator-bundle/test_render_templates.sh
@@ -2,9 +2,9 @@
 
 # get current values from upstream_sources.yaml
 export CI_SELF_NODE_REMEDIATION_UPSTREAM_URL=https://github.com/medik8s/self-node-remediation
-export CI_SELF_NODE_REMEDIATION_UPSTREAM_COMMIT=0eb8d6ca7aa1e65371bcc4e6f89fde3758341455
+export CI_SELF_NODE_REMEDIATION_UPSTREAM_COMMIT=ba384c8956e93413c63dd9abdb3e70d36a70df06
 
 # see gcp_env/product_version
-export CI_VERSION=0.4.0
+export CI_VERSION=0.5.0
 
 ./render_templates.in
